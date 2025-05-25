17c4eeff59a1e704dd2b8dd3767309d6fd2426c2
JiHwanLee <kdgosu53@gmail.com>
2025-05-25 16:32:55 +0900

    playwright 포트 충돌난 부분 수정

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
     },
     {
       command: 'node server_e2e.js',
-      port: 3000,
+      port: 3001,
       reuseExistingServer: !process.env.CI,
     },
   ],
--- server_e2e.js
+++ server_e2e.js
@@ -3,7 +3,7 @@ import { randomUUID } from 'crypto';
 import express from 'express';
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.json());
 
